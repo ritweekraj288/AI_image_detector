4418c1e1439abee293b806b5140290b68a119d61
ritweekraj288 <ritweekraj288@gmail.com>
2025-12-25 10:16:19 +0530

    Update import to use load_model function

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,8 +1,9 @@
 from PIL import Image
 import torch
-from app.model import image_processor, DEVICE
+from app.model import load_model, DEVICE
 
 def preprocess_image(image: Image.Image):
+    _, image_processor = load_model()
     image = image.convert("RGB")
     inputs = image_processor(image, return_tensors="pt")
     return inputs["pixel_values"].to(DEVICE)
